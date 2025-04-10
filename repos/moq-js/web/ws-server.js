8ecba402895201a46818a66091c8379a14bd9fe8
basaryumak <basaryumak@icloud.com>
2025-04-10 23:55:01 +0300

    websocket server basic implementation

---
+++ repos/moq-js/web/ws-server.js
@@ -0,0 +1,17 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+	console.log("Client connected");
+
+	ws.on("message", (message) => {
+		console.log("Received:", message.toString());
+	});
+
+	ws.on("close", () => {
+		console.log("Client disconnected");
+	});
+});
+
+console.log("WebSocket server running on ws://localhost:8080");
